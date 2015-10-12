31403a75fbf405df93f27db38a45559d1e3cb0c6
Harold Murcia <hl.murcia222@uniandes.edu.co>
2015-10-12 18:39:08 -0500

    R13 GUI 2HORAS

--- MusicStore.web/src/main/webapp/src/modules/purchase/purchase.ctrl.js
+++ MusicStore.web/src/main/webapp/src/modules/purchase/purchase.ctrl.js
@@ -10,6 +10,25 @@
                     $scope.purchases = res;
                 });
             };
+            $scope.sayHi = function() {
+                alert('hi!')
+            };
             getUserPurchases();
         }]);
+     ng.module('mainApp').directive('ngConfirmClick', [
+        function(){
+            return {
+                priority: 1,
+                terminal: true,
+                link: function (scope, element, attr) {
+                    var msg = attr.ngConfirmClick || "Are you sure?";
+                    var clickAction = attr.ngClick;
+                    element.bind('click',function (event) {
+                        if ( window.confirm(msg) ) {
+                            scope.$eval(clickAction)
+                        }
+                    });
+                }
+            };
+    }]);
 })(window.angular);
