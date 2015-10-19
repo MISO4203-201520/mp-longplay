6c1637cd671f8f0da234056d1e891178eb9b4eab
je.barrera11 <je.barrera11@MISOEST-06.virtual.uniandes.edu.co>
2015-10-18 21:41:03 -0500

    Ajuste de hallazgos en src/main/webapp/src/app.js

--- MusicStore.web/src/main/webapp/src/modules/cartItem/cartItem.ctrl.js
+++ MusicStore.web/src/main/webapp/src/modules/cartItem/cartItem.ctrl.js
@@ -1,10 +1,19 @@
-(function (ng) {
+(function(ng) {
+
     var mod = ng.module('cartItemModule');
 
-    mod.controller('cartItemCtrl', ['CrudCreator', '$scope', 'cartItemService', 'cartItemModel','purchaseService','$http', function (CrudCreator, $scope, svc, model, purchaseSvc,$http) {
-            CrudCreator.extendController(this, svc, $scope, model, 'cartItem', 'CartItem');         
+    var cICtrl = 'cartItemCtrl';
+    var cc = 'CrudCreator';
+    var cIServ = 'cartItemService';
+    var cIMod = 'cartItemModel';
+    var ps = 'purchaseService';
+
+    mod.controller(cICtrl, [cc, '$scope', cIServ, cIMod, ps,
+        function(CrudCreator, $scope, svc, model, purchaseSvc) {
+
+            CrudCreator.extendController(this, svc, $scope, model, 'cartItem', 'CartItem');
             var self = this;
-            
+
             $scope.paymentMethod = [{
                     value: 'deb',
                     label: 'Debit'
@@ -17,16 +26,18 @@
                 }, {
                     value: 'pay',
                     label: 'PayPal'
-                }]; 
-            
-            $scope.fecha="";
+                }];
+
+            $scope.fecha = "";
             var oldFetch = this.fetchRecords;
-            this.fetchRecords = function(){
-                return oldFetch.call(this).then(function(data){
+
+            this.fetchRecords = function() {
+                return oldFetch.call(this).then(function(data) {
                     self.calcTotal();
                     return data;
                 });
             };
+
             this.fetchRecords();
             this.readOnly = true;
             $scope.lastQuantity = 0;
@@ -37,41 +48,41 @@
                     displayName: ' ',
                     icon: 'trash',
                     class: 'primary',
-                    fn: function (record) {
-                        svc.deleteRecord(record).then(function(){
+                    fn: function(record) {
+                        svc.deleteRecord(record).then(function() {
                             self.fetchRecords();
                         });
                     },
-                    show: function () {
+                    show: function() {
                         return true;
                     }
                 }
             };
 
-            this.calcTotal = function () {
-                $scope.cartList=[];
+            this.calcTotal = function() {
+                $scope.cartList = [];
                 $scope.total = 0;
-                $scope.totalDiscount=0;
-                $scope.totalIva=0;
+                $scope.totalDiscount = 0;
+                $scope.totalIva = 0;
                 for (var i = 0; i < $scope.records.length; i++) {
-                    var iva=($scope.records[i].longPlay.price * $scope.records[i].quantity)*0.16;
-                    var total= (($scope.records[i].longPlay.price * $scope.records[i].quantity)+iva)-$scope.records[i].longPlay.discount;
+                    var iva = ($scope.records[i].longPlay.price * $scope.records[i].quantity) * 0.16;
+                    var total = (($scope.records[i].longPlay.price * $scope.records[i].quantity) + iva) - $scope.records[i].longPlay.discount;
                     $scope.total += total;
                     $scope.totalDiscount += $scope.records[i].longPlay.discount;
                     $scope.totalIva += iva;
-                    var obj=new Object();
-                    obj.price=$scope.records[i].longPlay.price;
-                    obj.quantity=$scope.records[i].quantity;
-                    obj.longPlay=$scope.records[i].longPlay;
+                    var obj = {};
+                    obj.price = $scope.records[i].longPlay.price;
+                    obj.quantity = $scope.records[i].quantity;
+                    obj.longPlay = $scope.records[i].longPlay;
                     $scope.cartList.push(obj);
                 }
             };
 
-            $scope.verify = function (quantity) {
+            $scope.verify = function(quantity) {
                 $scope.lastQuantity = quantity;
             };//guarda la cantidad anterior
 
-            $scope.postVerify = function (record) {
+            $scope.postVerify = function(record) {
                 var patron = /^\d*$/; //^[0-9]{3}$
                 if (patron.test(record.quantity) && record.quantity > 0) {
                     self.calcTotal();
@@ -81,80 +92,81 @@
                     $scope.currentRecord = record;
                 }
             };//Realiza la validacion de la nueva cantidad asignada.
-            $scope.checkout = function () {
-                
-                //self.showWarning("Not implemented yet");
+
+            $scope.checkout = function() {
                 $('#divPagar').show('slow');
             };
-            $scope.subtotal = function (record) {
-                var iva=(record.longPlay.price * record.quantity)*0.16;
-                return ((record.longPlay.price * record.quantity)+iva)-record.longPlay.discount;
+
+            $scope.subtotal = function(record) {
+                var iva = (record.longPlay.price * record.quantity) * 0.16;
+                return ((record.longPlay.price * record.quantity) + iva) - record.longPlay.discount;
             };
-            $scope.iva = function (record) {
-                var iva=(record.longPlay.price * record.quantity)*0.16;
+
+            $scope.iva = function(record) {
+                var iva = (record.longPlay.price * record.quantity) * 0.16;
                 return iva;
             };
-            
-            
-           function fecha(num){
-               if(num.toString().length==1){
-                   return "0"+num;
-               }
-               
-               return num;
-           }
-           $scope.comboChange = function () {
-               if ($scope.paymentMethodList.label=="Credit"){
-                   $("#divCredito").show("slow");
-               }else{
-                   
-                   $("#divCredito").hide("slow");
-                   
-               }
-           };
-            
-            $scope.pay = function () {
-                var date="";
-                var grabar=true;
-                if ($scope.cardNumber == undefined){
-                    self.showWarning("Number is required.");                    
-                }else if($scope.paymentMethodList==undefined){
-                    self.showWarning("Payment method is required.");
-                } else{
-                    if ($scope.paymentMethodList.label=="Credit"){
-                   if ($scope.expire==undefined || $scope.nameCardOwner=="" || $scope.nameCardOwner==undefined || $scope.cvc=="" || $scope.cvc==undefined){
-                       self.showWarning("Missing data.");
-                       grabar=false;
-                   }else{
-                       date=($scope.expire.getMonth()+1)+"/"+$scope.expire.getFullYear();
-                   }
-                    }
-                    if (grabar==true){
-                    var f = new Date();
-                    var pay =new Object();
-                    pay.date= f.getFullYear()+ "-" +fecha((f.getMonth() +1)) + "-" +fecha(f.getDate());
-                    pay.iva=$scope.totalIva;                    
-                    pay.paymentMethod=$scope.paymentMethodList.label;
-                    pay.cardNumber=$scope.cardNumber;
-                    pay.total=$scope.total;
-                    pay.purchaseDetail=$scope.cartList;
-                    pay.expirationDate=date;
-                    pay.CVC=$scope.cvc;
-                    pay.nameCardOwner=$scope.nameCardOwner;
-                    //pay.nameCardOwner=$scope.nameCardOwner;
-                    purchaseSvc.addItem(pay);
-                   for (var i = 0; i < $scope.records.length; i++) {
-                   svc.deleteRecord($scope.records[i]).then(function(){
-                            //self.fetchRecords();
-                        });
-                   }
-                   alert("La Compra se ha realizado de manera exitosa!");
-                //$('#divPagar').hide('slow');    
-                window.location="#/catalog";
-                 }
+
+            function fecha(num) {
+                if (num.toString().length === 1) {
+                    return "0" + num;
+                }
+                return num;
+            }
+
+            $scope.comboChange = function() {
+                if ($scope.paymentMethodList.label === "Credit") {
+                    $("#divCredito").show("slow");
+                } else {
+                    $("#divCredito").hide("slow");
                 }
             };
-            
-           
+
+            $scope.pay = function() {
+
+                var date = "";
+                var grabar = true;
+                if ($scope.cardNumber === undefined || $scope.paymentMethodList === undefined) {
+                    self.showWarning("Number and Payment Method is required.");
+                } else {
+                    if ($scope.paymentMethodList.label === "Credit") {
+                        if ($scope.expire === undefined) {
+                            self.showWarning("Expired data is required");
+                            grabar = false;
+                        }
+                        else if ($scope.nameCardOwner === "" || $scope.nameCardOwner === undefined) {
+                            self.showWarning("Card data is required");
+                            grabar = false;
+                        }
+                        else if ($scope.cvc === "" || $scope.cvc === undefined) {
+                            self.showWarning("Data is required");
+                            grabar = false;
+                        }
+                        else {
+                            date = ($scope.expire.getMonth() + 1) + "/" + $scope.expire.getFullYear();
+                        }
+                    }
+                    if (grabar === true) {
+                        var f = new Date();
+                        var pay = {};
+                        pay.date = f.getFullYear() + "-" + fecha((f.getMonth() + 1)) + "-" + fecha(f.getDate());
+                        pay.iva = $scope.totalIva;
+                        pay.paymentMethod = $scope.paymentMethodList.label;
+                        pay.cardNumber = $scope.cardNumber;
+                        pay.total = $scope.total;
+                        pay.purchaseDetail = $scope.cartList;
+                        pay.expirationDate = date;
+                        pay.CVC = $scope.cvc;
+                        pay.nameCardOwner = $scope.nameCardOwner;
+                        purchaseSvc.addItem(pay);
+                        for (var i = 0; i < $scope.records.length; i++) {
+                            svc.deleteRecord($scope.records[i]).then(function() {
+                            });
+                        }
+                        alert("Congratulations, purchase made successfully!");
+                        window.location = "#/catalog";
+                    }
+                }
+            };
         }]);
 })(window.angular);
