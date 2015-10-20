c55e28153ad6a8d53864a0eded82d37483962371
je.barrera11 <je.barrera11@MISOEST-06.virtual.uniandes.edu.co>
2015-10-20 00:45:32 -0500

    Ajuste de hallazgos de JS

--- MusicStore.web/src/main/webapp/src/modules/longPlay/catalog.ctrl.js
+++ MusicStore.web/src/main/webapp/src/modules/longPlay/catalog.ctrl.js
@@ -23,7 +23,6 @@
 
             $scope.modal = {url: 'src/modules/question/question.tpl.html', data: null, question: ''};
 
-
             this.searchByName = function(albumName) {
                 var search;
                 if (albumName) {
@@ -33,8 +32,7 @@
             };
 
             this.registerQuestion = function() {
-                var result = false;
-                result = questionService.registerQuestion($scope.modal.data, $scope.modal.question);
+                var result = questionService.registerQuestion($scope.modal.data, $scope.modal.question);
                 if (result) {
                     $scope.modal.data = null;
                     $scope.modal.question = '';
@@ -46,7 +44,6 @@
                 {
                     commentService.createComment(record, commentReg, null);
                     this.answerMode = false;
-                    this.refreshComment(record);
                 }
                 $scope.commentReg = "";
                 this.refreshComment(record);
@@ -62,54 +59,32 @@
 
             this.findItem = function(priceMax) {
                 svc.findItem(priceMax).then(function(results) {
-                    $scope.records = [];
                     $scope.records = results;
                 });
             };
 
             this.playSong = function(record, song) {
-
-                jwplayer("player" + record.id).setup({
-                    playlist: [
-                        {
-                            file: song.sample,
-                            title: song.title,
-                            image: record.album.cover,
-                            description: record.comments
-                        }
-                    ]
-                });
+                jwplayer("player" + record.id).setup({playlist: [{file: song.sample, title: song.title, image: record.album.cover, description: record.comments}]});
             };
+
             this.addAnswer = function(record, answer) {
                 if (answer.trim().length !== 0)
                 {
                     commentService.createComment(record, answer, this.idCommentPadre);
                     this.answerMode = false;
-                    this.refreshComment(record);
                 }
                 this.refreshComment(record);
             };
-            this.recordActions = [{
-                    name: 'addToCart',
-                    displayName: 'Add to Cart',
-                    icon: 'shopping-cart',
-                    class: 'primary',
+            
+            this.recordActions = [{name: 'addToCart', displayName: 'Add to Cart', icon: 'shopping-cart', class: 'primary',
                     fn: function(longPlay) {
-                        return cartItemSvc.addItem({
-                            longPlay: longPlay,
-                            name: longPlay.name,
-                            quantity: 1
-                        });
+                        return cartItemSvc.addItem({longPlay: longPlay, name: longPlay.name, quantity: 1});
                     },
                     show: function() {
                         return true;
                     }
                 },
-                {
-                    name: 'addQuestion',
-                    displayName: 'Add Question',
-                    icon: 'question-sign',
-                    class: 'info',
+                {name: 'addQuestion', displayName: 'Add Question', icon: 'question-sign', class: 'info',
                     fn: function(record) {
                         $scope.modal.data = record;
                         $scope.modal.question = '';
@@ -119,12 +94,7 @@
                         return true;
                     }
                 },
-                {
-                    name: 'comments',
-                    displayName: 'Comments',
-                    icon: 'list',
-                    class: 'info',
-                    fn: function(record) {
+                {name: 'comments', displayName: 'Comments', icon: 'list', class: 'info', fn: function(record) {
                         svc.api.get(record.id).then(function(data) {
                             self.detailsMode = true;
                             self.findCheapMode = false;
@@ -135,12 +105,7 @@
                         return !self.detailsMode;
                     }
                 },
-                {
-                    name: 'songList',
-                    displayName: 'Song List',
-                    icon: 'music',
-                    class: 'info',
-                    fn: function(record) {
+                {name: 'songList', displayName: 'Song List', icon: 'music', class: 'info', fn: function(record) {
                         if (record.songs.length > 0)
                             $('#song_modal_' + record.id).modal('show');
                         return true;
